e75439bc76cb53454522b673d873f12df530f39e
Anthony Bush <anthony@anthonybush.com>
2011-09-02 02:18:59 -0500

    Fixes #1: map the 0, 0 that seems to correspond to the bottom left of the main screen so that 0, 0 is the top left.

--- display_lister.m
+++ display_lister.m
@@ -2,11 +2,26 @@
 
 int main(int argc, const char* argv[])
 {
+	double yOffset = 0;
+	int i = 0;
 	for (NSScreen *screen in [NSScreen screens])
 	{
 		NSSize size = [screen frame].size;
 		NSPoint origin = [screen frame].origin;
-		printf("%g %g %g %g\n", size.width, size.height, origin.x, origin.y);
+		
+		// map the 0, 0 that seems to correspond to the bottom left of the main screen so that 0, 0 is the top left (to match the format given by AppleScript provided window positions)
+		double yOrigin = 0;
+		if (i == 0)
+		{
+			yOffset = size.height;
+		}
+		else
+		{
+			yOrigin = yOffset - origin.y - size.height;
+		}
+		
+		printf("%g %g %g %g\n", size.width, size.height, origin.x, yOrigin);
+		++i;
 	}
 	return 0;
 }
